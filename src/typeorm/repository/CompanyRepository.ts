14232fb5cc831d120925a9dcdccdd759a7a29353
diegodarossi <42331236+diegodarossi@users.noreply.github.com>
2021-07-21 14:33:07 -0300

    Update CompanyRepository.ts

--- src/typeorm/repository/CompanyRepository.ts
+++ src/typeorm/repository/CompanyRepository.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 import { Company } from 'src/shared/entity/Company';
-import { UserValidator } from 'src/shared/validator/UserValidator';
+import { CompanyValidator } from 'src/shared/validator/CompanyValidator';
 import { EntityRepository } from "typeorm";
 import { CompanyConfiguration } from '../configuration/CompanyConfiguration';
 import { BaseRepository } from "./BaseRepository";
@@ -10,6 +10,6 @@ export class CompanyRepository extends BaseRepository<Company> {
 
     public validate(entity: Company): Joi.ValidationResult
     {
-        return UserValidator.validate(entity);
+        return CompanyValidator.validate(entity);
     }
 }
